2be94f733a548fb601b34e45e1ffb3a925a26665
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-12 21:52:46 -0600

    added achievements

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -3,6 +3,12 @@ import styled from "styled-components";
 import Overdrive from "react-overdrive";
 import logo from "./js.png";
 import AnimatedMap from "../Map";
+import cu from "./cu.png";
+import fullstack from "./fullstack.png";
+import gGrad from "./gGrad.png";
+import mmgr from "./mmgr.png";
+import nerd from "./nerd.png";
+import skills from "./skills.png";
 
 const About = () => (
   <TheAbout>
@@ -12,7 +18,7 @@ const About = () => (
     <span>
       {/* Logo animation */}
       <Overdrive id="img" duration="700">
-        <img src={logo} alt="JM Logo" />
+        <img id="logo" src={logo} alt="JM Logo" />
       </Overdrive>
     </span>
     <div>
@@ -25,10 +31,19 @@ const About = () => (
       <p id="places-visited">Here you can browse all the places I visited during my 11 years of travel on cruise ships!</p>
 
       <AnimatedMap />
+      <h2>JAMES'S ACHIEVEMENTS AT A GLANCE</h2>
+      <div id="achievements">
+        <img id="top-achievement" src={fullstack} alt="fullstack" />
+        <img src={skills} alt="skills" />
+        <img src={mmgr} alt="mmgr" />
+        <img src={gGrad} alt="gGrad" />
+        <img src={cu} alt="cu" />
+        <img src={nerd} alt="nerd" />
+      </div>
 
       <p>Prior to my transition into the developer world, I had been playing music professionally for 15 years and for the last 11 years I was traveling the world as a Music Director/Manager/Musician (Saxophones, Flute, Clarinet).</p>
       <p> Working on cruise ships, I have lived and worked with people from up to 60 different countries, backgrounds and native languages.  I believe the ability to communicate effectively and work together regardless of where a person is from and the language they speak is an invaluable skill that I have developed. This applies when working with customers and co-workers as well as in the creation of web applications that appeal to a specific demographic or a larger worldview.</p>
-      <p>In addition to performing, I have also supported the Colorado music scene through education.  I ran a private lesson studio with 22 students, I have been a guest instructor for middle and high schools as well as a guest clinician.  My passion for teaching has extended into the developer community as well.  I am a co-organiser of Learn to Code at Galvanize, a weekly 2 hour class where I teach basic HTML, CSS and JavaScript.  I am a Teaching Assstant for the JavaScript Accelerated course at Galvanize as well.  A classmate and I developed a weekly JavaScript whiteboard practice session to help with interview preparation we call Beer Boarding.  Lastly, I co-developed and taught a 3 hour course on HTML, CSS and Materialize for Brunch and Learn as well as a 3 part JavaScript for Beginners course that runs for 3 hours on Saturdays.</p>
+      <p>In addition to performing, I have also supported the Colorado music scene through education.  I ran a private lesson studio with 22 students, I have been a guest instructor for middle and high schools as well as a guest clinician.  My passion for teaching has extended into the developer community as well.  I am a co-organizer of Learn to Code at Galvanize, a weekly 2 hour class where I teach basic HTML, CSS and JavaScript.  I am a Teaching Assistant for the JavaScript Accelerated course at Galvanize as well.  A classmate and I developed a weekly JavaScript whiteboard practice session to help with interview preparation we call Beer Boarding.  Lastly, I co-developed and taught a 3 hour course on HTML, CSS and Materialize for Brunch and Learn as well as a 3 part JavaScript for Beginners course that runs for 3 hours on Saturdays.</p>
       <p>I am currently enrolled in the Web Development Immersive program at Galvanize - Denver, Platte Campus and will graduate in April 2018. Prior to Galvanize, I studied at the University of Colorado, Boulder and received Bachelor&#39;s degrees in Music Performance (Saxophone, Jazz studies) and Music Education. I am a lifelong student of the martial arts and have studied Judo, Wrestling, Arnis/Eskrima, Shaolin Kung-Fu and most recently, Systema. In my free time I am a snowboarder, fitness enthusiast and gamer.</p>
     </div>
   </TheAbout>
@@ -80,6 +95,18 @@ h2 {
   text-align: center;
 }
 
+#achievements {
+  display: flex;
+  flex-direction: column;
+  #top-achievement {
+    margin-top: 1.5rem;
+  }
+  img {
+    margin-bottom: 1.5rem;
+    width: 100%;
+  }
+}
+
 p {
   font-size: 20px;
 }
@@ -106,7 +133,7 @@ p {
 
 @media (max-width: 666px) {
   padding-top: 70px;
-    img {
+    #logo {
       display: none;
     }
 
